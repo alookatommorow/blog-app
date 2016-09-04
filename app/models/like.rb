9a917abc4027a3c575a084ebb460c695a63d1297
alookatommorow <hessjohnt@gmail.com>
2016-09-04 15:03:18 -0700

    add validations to user

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,4 +2,7 @@ class Like < ActiveRecord::Base
   belongs_to :likeable, polymorphic: true
   belongs_to :user
 
+  validates :user_id, presence: true
+  validates :likeable_id, presence: true
+  validates :likeable_type, presence: true
 end
