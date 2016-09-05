309c7e673d1a85b69f7d767cebcb9c13e202c3bd
alookatommorow <hessjohnt@gmail.com>
2016-09-05 10:28:54 -0700

    add movies actions and views

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,10 @@
 class MoviesController <ApplicationController
+  def index
+    @movies = Movie.all
+  end
 
-
+  def show
+    @movie = Movie.find(params[:id])
+    @likes = Like.where(likeable_id: params[:id]).count
+  end
 end
