385ad92c1a4ef7514808d8913cf23dbaaf35a245
alookatommorow <hessjohnt@gmail.com>
2016-09-05 18:40:06 -0700

    add like button and num likes ajax

--- app/assets/javascripts/likes.js
+++ app/assets/javascripts/likes.js
@@ -1,17 +1,21 @@
 $(document).ready(function() {
 
-  $(".like-form").submit(function(event) {
+  $(".like-form-container").on("submit", ".like-form", function(event) {
     event.preventDefault();
     $.ajax({
       url: this.action,
       method: this.method,
       data: $(this).serialize(),
     })
-    .done(function() {
-      console.log("fuck ya");
-    })
+    .done(renderLikes.bind(this) )
     .fail(function() {
       console.log("bitch ass");
     })
-  })
+  });
+
+  function renderLikes(response) {
+    var $container =  $(this).data("ajax-update");
+    $(this).closest("[data-button-container]").html(response.button);
+    $($container).html(response.likes);
+  }
 });
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,12 @@ class LikesController <ApplicationController
   def create
     @like = Like.new(like_params)
     @like.save
-    render_like_button
+    render_likes
   end
 
   def destroy
     Like.destroy(params[:id])
-    render_like_button
+    render_likes
   end
 
   private
@@ -16,7 +16,22 @@ class LikesController <ApplicationController
     params.require(:like).permit(:likeable_id, :likeable_type, :user_id)
   end
 
-  def render_like_button
-    render partial: "like_button", locals: { movie: Movie.find(like_params[:likeable_id]), like: @like }
+  def render_likes
+    render json: {
+      button: render_to_string('_like_button.html.erb',
+        layout: false,
+        locals: {
+          movie: Movie.find(like_params[:likeable_id]),
+          like: @like
+        }
+      ),
+      likes: render_to_string('_likes.html.erb',
+        layout: false,
+        locals: {
+          likes:  Like.where(likeable_id: like_params[:likeable_id]).count
+        }
+      ),
+    }
+    # render partial: "like_button", locals: { movie: Movie.find(like_params[:likeable_id]), like: @like }
   end
 end
